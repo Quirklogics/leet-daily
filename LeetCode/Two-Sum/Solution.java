c8fe3aa528152a65fe25b8315cf9f5c616fec2ed
S U P R E M E <sandeshmajhi446@gmail.com>
2026-09-18 18:13:36 +0545

    Add solution for Two Sum

--- LeetCode/Two-Sum/Solution.java
+++ LeetCode/Two-Sum/Solution.java
@@ -1,9 +1,16 @@
-1class Solution:
-2    def twoSum(self, nums: List[int], target: int) -> List[int]:
-3        dict = {}
-4        for i, value in enumerate(nums):
-5            req = target - value
-6            if req in dict:
-7                return [dict[req], i]
-8            dict[value] = i
-9        return []
+1class Solution {
+2    public int[] twoSum(int[] nums, int target) {
+3        int ptr=nums.length-1;
+4     for(int i = 0; i<nums.length; i++) {
+5     for(int j=i+1; j<nums.length;j++)
+6 {
+7            if(nums[i]+nums[j]==target)
+8            {
+9            return new int[]{i, j};
+10     } 
+11
+12    }   
+13      }
+14     return new int[]{};
+15    }
+16}
